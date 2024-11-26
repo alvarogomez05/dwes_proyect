6774b1d2450bd37fefb7e4b9326a692080c264e0
alvarogomez05 <alvatox11@gmail.com>
2024-11-26 18:03:00 +0100

    admin2

--- pages/admin.php
+++ pages/admin.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ADMIN</title>
-    <link rel="stylesheet" href="admin.css">
+    <link rel="stylesheet" href="../css/admin.css">
 </head>
 
 <body>
@@ -18,7 +18,18 @@
     <!-- header -->
     <header class="header">
         <!-- imagen de perfil para el usuario -->
-        <img src="" alt="">
+        <img class="header__img" src="" alt="">
+
+        <div class="header__menu">
+            <nav class="header__nav">
+                <ul class="ul__list">
+                    <li class="list___item"><a href="./admin.php" class="list__link">home</a></li>
+                    <li class="list___item"><a href="#" class="list__link">circuitos</a></li>
+                    <li class="list___item"><a href="#" class="list__link">motos</a></li>
+                    <li class="list___item"><a href="#" class="list__link">pilotos</a></li>
+                </ul>
+            </nav>
+        </div>
     </header>
     <div class="container">
 
