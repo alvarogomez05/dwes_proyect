aa69761f0cea614e340eadd045f1f9f0a4fd1684
ricardo <rihozmar@gmail.com>
2024-11-27 20:01:46 +0100

    EStilos

--- pages/admin.php
+++ pages/admin.php
@@ -5,10 +5,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ADMIN</title>
-    <link rel="stylesheet" href="../css/admin.css">
+
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Jaro:opsz@6..72&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="./../css/admin.css">
 </head>
 
 <body>
