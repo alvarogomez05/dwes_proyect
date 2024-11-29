c3e97b458fdd9e37663aca3b97b2076d896a234d
alvarogomez05 <alvatox11@gmail.com>
2024-11-29 20:32:54 +0100

    estilos tabla

--- pages/pilotos.php
+++ pages/pilotos.php
@@ -5,12 +5,46 @@ include "./funciones.php";
 //conectamos la bdd
 conectar();
 
-//hacemos la consulta
 try {
     $sql = "SELECT * FROM piloto";
     $lista = $pdo->query($sql);
+
+    // Estilos CSS para la tabla
+    echo "<style>
+        table {
+            width: 70%;
+            border-collapse: collapse;
+            box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.1); /* sombra de la tabla */
+            margin: 3rem auto;
+        }
+        th, td {
+            border: 1px solid black; /* Bordes negros */
+            padding: 8px;
+            text-align: left;
+        }
+        thead {
+            background-color: red; /* Fondo rojo para el encabezado */
+            color: white; /* Color blanco para el texto del encabezado */
+        }
+        tbody tr:nth-child(even) {
+            background-color: #f2f2f2; /* Fondo gris claro para filas pares */
+        }
+        tbody tr:hover {
+            background-color: #ddd; /* Fondo gris más oscuro cuando se pasa el mouse sobre una fila */
+        }
+    </style>";
+
     // Definición correcta de la tabla
-    echo "<table><thead><tr><th>Nombre</th><th>Apellido</th><th>Edad</th><th>Puntos</th></tr></thead><tbody>";
+    echo "<table>
+            <thead>
+                <tr>
+                    <th>Nombre</th>
+                    <th>Apellido</th>
+                    <th>Edad</th>
+                    <th>Puntos</th>
+                </tr>
+            </thead>
+            <tbody>";
 
     // Recorriendo la lista de pilotos
     while ($piloto = $lista->fetch()) {
--- pages/user.php
+++ pages/user.php
@@ -102,7 +102,7 @@
             </article>
         </main>
     </div>
-    <div>
+    <div id="lista_pilotos" style="width: 1200px; margin:0 auto;">
         <?php
         include_once "pilotos.php";
         ?>
