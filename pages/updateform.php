bb656bfa51da7769ee522eec3a2971cf2a944cdb
ricardo <rihozmar@gmail.com>
2024-12-03 18:00:01 +0100

    Estilos formupdate

--- pages/updateform.php
+++ pages/updateform.php
@@ -15,32 +15,36 @@ if (isset($_GET['nombre']) && isset($_GET['apellido'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Jaro:opsz@6..72&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="./../css/form_update.css">
     <title>Datos del piloto</title>
 </head>
 
 <body>
     <!-- el action nos enviara a la página que actualizará los datos en la bdd y posteriormente redireccionara a la pagina admin.php -->
-    <form action="update.php" method="post">
+    <form action="update.php" method="post" class="form_update">
         <div>
-            <label for="">Nombre</label>
+            <label for="">Nombre:</label>
             <!-- nombre del piloto -->
             <span><?php
-                    echo $nombre;
+                    echo "Ricardo";
+                    // echo $nombre;
                     ?></span>
             <input type="hidden" name="nombre" value="<?php
                                                         //el value tamben es el nombre del piloto
-                                                        echo $nombre;
+                                                        // echo $nombre;
                                                         ?>">
         </div>
         <div>
-            <label for="">Apellidos</label>
+            <label for="">Apellidos:</label>
             <!-- apellidos del piloto -->
             <span><?php
-                    echo $apellido;
+                    echo "de la Hoz";
+                    // echo $apellido;
                     ?></span>
             <input type="hidden" name="apellidos" value="<?php
                                                             //el value tambien es el apellido del piloto
-                                                            echo $apellido;
+                                                            // echo $apellido;
                                                             ?>">
         </div>
         <div>
@@ -65,8 +69,10 @@ if (isset($_GET['nombre']) && isset($_GET['apellido'])) {
             </select>
         </div>
 
-        <button type="submit">enviar</button>
-        <button type="reset">borrar</button>
+        <div class="container_buttons">
+            <button type="submit">enviar</button>
+            <button type="reset">borrar</button>
+        </div>
 
     </form>
 
